4364371f0101d2616c9c628db4c3d6c9d8503640
SunSeaLucky <sunsealucky@gmail.com>
2024-08-11 23:20:23 +0800

    ADD basic Spring Boot framework

---
+++ DataSphereServer/src/main/java/prv/sunsealucky/datashpere/server/datasphereserver/DataSphereServerApplication.java
@@ -0,0 +1,13 @@
+package prv.sunsealucky.datashpere.server.datasphereserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSphereServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSphereServerApplication.class, args);
+    }
+
+}
---
+++ DataSphereServer/src/test/java/prv/sunsealucky/datashpere/server/datasphereserver/DataSphereServerApplicationTests.java
@@ -0,0 +1,13 @@
+package prv.sunsealucky.datashpere.server.datasphereserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSphereServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
